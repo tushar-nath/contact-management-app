2a1718ae67512ad0c1d0976ace048bc6d133d166
tusharnath10 <tusharnath10@gmail.com>
2023-05-27 22:29:40 +0530

    Initialised the project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
